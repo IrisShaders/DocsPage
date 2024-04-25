29c7c994a7d92d0c89e61e2075efaf7d578db767
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-04-24 22:08:16 -0700

    added the new site version

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Iris Docs',
+			customCss: [
+				'./src/styles/root.css',
+			],
+			social: {
+				github: 'https://github.com/IrisShaders/ShaderDoc/',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
