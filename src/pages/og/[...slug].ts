5d2035adbb0adeaca42ef333bc5ab75c5abe8cd0
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-05-02 11:59:26 -0700

    mod: social: changed side color in  ts file

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -13,11 +13,11 @@ export const { getStaticPaths, GET } = OGImageRoute({
   param: 'slug',
   getImageOptions: (_path, page: (typeof pages)[number]) => {
     return {
-      title: page.data.title,
-      description: page.data.description,
+      title: "Iris Shader Docs",
+      description: page.data.title,
       backgroundImage: `linear-gradient(rgba(24, 24, 27, 0.4), rgba(24, 24, 27, 0.4)), url('/assets/banner.png')`,
       bgGradient: [[24, 24, 27]],
-      border: { color: [63, 63, 70], width: 20 },
+      border: { color: [76, 108, 178], width: 20 },
       padding: 120,
     }
   },
