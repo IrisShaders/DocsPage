349a17bf716ede097847530d159ef726ea76cfff
Josh Britain <50422789+jbritain@users.noreply.github.com>
2024-08-18 17:58:08 +0100

    test 2

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -8,10 +8,11 @@ const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]))
 export const { getStaticPaths, GET } = OGImageRoute({
   pages,
   param: 'slug',
-  getImageOptions: (_path, page: (typeof pages)[number]) => {
+  getImageOptions: (_path: string, page: (typeof pages)[number]) => {
     return {
       title: page.data.title,
       description: page.data.description,
+      logo: { path: './src/assets/og_banner.png', size: [350] },
       backgroundImage: `linear-gradient(rgba(24, 24, 27, 0.4), rgba(24, 24, 27, 0.4)), url('/assets/banner.png')`,
       bgGradient: [[24, 24, 27]],
       border: { color: [76, 108, 178], width: 20 },
